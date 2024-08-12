9203523bc630691ada3f8235391dcd7981a68186
Aryanvishwanath4 <aryanvaryan16@gmail.com>
2024-08-12 12:28:17 +0530

    CRUD operations on task

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response, NextFunction } from "express";
+import taskRoutes from "./routes/tasks";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use("/tasks", taskRoutes);
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello Aryan");
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack);
+  res.status(500).send("Something went wrong");
 });
 
 app.listen(port, () => {
